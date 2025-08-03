b3dc545955e869c8bb2c5b31c0e3a58358c3d64b
quddus larik <tg53rt3@gmail.com>
2025-08-03 11:08:17 +0500

    setup folders

---
+++ error handling/tryex.py
@@ -0,0 +1,8 @@
+inc = 0
+try:
+  if(error):
+    print("error not found")
+except:
+  raise print("error occured!")
+finally:
+  inc += 1
--- errors/tryex.py
+++
@@ -1,4 +0,0 @@
-x = "hello"
-
-if not type(x) is int:
-  raise TypeError("styntax err: integers are allowed")
